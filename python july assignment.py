a2171e6eb3914e87ce53ad3da4f28e54f3078753
gpswarwal <99944302+gpswarwal@users.noreply.github.com>
2022-07-06 15:43:42 +0530

    Add files via upload

--- python july assignment.py
+++ python july assignment.py
@@ -3,9 +3,11 @@
 
 # 2. Write a Python program to get the Python version you are using.
 # from wsgiref.simple_server import sys_version
-"""print(sys_version)"""
+""" 
+print(sys_version)"""
 
-"""import sys
+"""
+import sys
 print(sys.version)"""
 
 #3. Write a Python program to display the current date and time.
@@ -27,18 +29,38 @@ print(x.strftime("%A")) #To show day of the week """
 # print(current_time.strftime("%A")) #This is ("%A") show the weekday
 
 #4. Write a Python program which accepts the radius of a circle from the user and compute the area.
-
-
-
-# x = int(input("Radius of circle= "))
-# y = (22/7)*x**2
-# print(y)
-
-# x = int(input("Radius of circle= "))
-# y = (3.14)*x**2
-# print(y)
-
+"""#1st
+x = int(input("Radius of circle= "))
+y = (22/7)*x**2
+print(y)
+#2nd
+x = int(input("Radius of circle= "))
+y = (3.14)*x**2
+print(y)
+#3rd 
 radius = int(input("Radius of circle= "))
 pi = 3.14
 area = pi*radius**2
-print(area)
+print(area)"""
+
+
+#5 Write a Python program which accepts the user's first and last name and 
+# print them in reverse order with a space between them
+"""a = "Gurpreet"
+b = "Singh"
+print(b,a)"""
+
+# 6. Write a Python program which accepts a sequence of comma-separated numbers from user and 
+# generate a list and a tuple with those numbers.
+values = input("Input some comma seprated numbers : ")
+list = values.split()
+tuple = tuple(list)
+print('List : ',list)
+print('Tuple : ',tuple)
+
+# #7. Write a Python program to accept a filename from the user and print the extension of that. Go to the editor
+# Sample filename : abc.java
+# Output : java
+
+# filename = input("Enter file name: ")
+# file_extns = filename.split(".")
